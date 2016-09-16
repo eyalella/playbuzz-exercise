781c45b7ebec5b0fe63b99fa1b1d230b90b2fa54
Eyal Ella <eyal@bigpanda.io>
2016-09-16 13:45:16 +0300

    Finished search route

--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -1,8 +1,18 @@
-angular.module('videoApp').controller('searchController', function ($sce, youtubeFactory) {
+angular.module('videoApp').controller('searchController', function ($scope, $sce, youtubeFactory) {
   var vm = this
-  vm.src = 'asd'
+  var YOUTUBE_BASE = 'https://www.youtube.com/embed/'
+
+  vm.src = ''
+  vm.query = ''
+  vm.selectedVideo = null
+
   vm.getMatches = getMatches;
-  console.log('asdsd');
+  vm.updateVideoSrc = updateVideoSrc;
+
+  function updateVideoSrc () {
+    vm.src = $sce.trustAsResourceUrl(YOUTUBE_BASE + vm.selectedVideo.id.videoId)
+  }
+
   function getMatches (searchText) {
     return youtubeFactory.getVideosFromSearchByParams({
       q: searchText,
--- src/directives/NavigationDirective.js
+++ src/directives/NavigationDirective.js
@@ -1,8 +1,5 @@
 angular.module('videoApp').directive('navigationDirective', function () {
   return {
-    templateUrl: 'src/views/navigation.html',
-    controller: function () {
-      console.log('adasfsdfdfffffff');
-    }
+    templateUrl: 'src/views/navigation.html'
   }
 })
